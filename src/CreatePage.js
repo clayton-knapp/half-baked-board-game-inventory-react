d187ac91e657ba63bf3a54880d4e5612d736eb37
Clayton Knapp <csjknapp@gmail.com>
2022-02-09 15:25:49 -0800

    create page working

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { createGame } from './services/fetch-utils';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function CreatePage() {
   // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
+  const history = useHistory();
 
   // here's the state you'll need:
     // title;
@@ -11,29 +13,53 @@ export default function CreatePage() {
     // description;
     // minPlayers;
     // maxPlayers;
+  const [titleForm, setTitleForm] = useState('');
+  const [genreForm, setGenreForm] = useState('');
+  const [designerForm, setDesignerForm] = useState('');
+  const [descriptionForm, setDescriptionForm] = useState('');
+  const [minPlayersForm, setMinPlayersForm] = useState(1);
+  const [maxPlayersForm, setMaxPlayersForm] = useState(1);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     // create a game
+    const game = {
+      title: titleForm,
+      genre: genreForm,
+      designer: designerForm,
+      description: descriptionForm,
+      min_players: minPlayersForm,
+      max_players: maxPlayersForm
+    };
+    const response = await createGame(game);
+    console.log(response);
 
     // use history.push to send the user to the list page
+    history.push('/board-games');
+
   }
 
   return (
     <div className='create'>
       {/* on submit, call your handleSubmit function */}
-      <form>
+      <form
+        onSubmit={handleSubmit}
+      >
         <h2>Add board game</h2>
         <label>
             Title
           {/* on change, set the title in state */}
-          <input required name='title' />
+          <input required name='title' 
+            onChange ={(e)=> setTitleForm(e.target.value)}
+          />
         </label>
         <label>
             Genre
           {/* on change, set the genre in state */}
-          <select required>
+          <select required
+            onChange ={(e)=> setGenreForm(e.target.value)}
+          >
             <option>Tile-laying</option>
             <option>Economic</option>
             <option>War</option>
@@ -46,22 +72,30 @@ export default function CreatePage() {
         <label>
             Designer
           {/* on change, set the designer in state */}
-          <input required name='designer' />
+          <input required name='designer' 
+            onChange ={(e)=> setDesignerForm(e.target.value)}
+          />
         </label>
         <label>
             Min Players
           {/* on change, set the min players in state */}
-          <input required name='min_players' />
+          <input required name='min_players' 
+            onChange ={(e)=> setMinPlayersForm(e.target.value)}
+          />
         </label>
         <label>
             Max Players
           {/* on change, set the max players in state */}
-          <input required name='max_players' />
+          <input required name='max_players' 
+            onChange ={(e)=> setMaxPlayersForm(e.target.value)}
+          />
         </label>
         <label>
             Description
           {/* on change, set the description in state */}
-          <textarea required name='max_players' />
+          <textarea required name='description' 
+            onChange ={(e)=> setDescriptionForm(e.target.value)}
+          />
         </label>
         <button>Create game</button>
       </form>
